505546a96047e85e28ef857a30d8c96b3b5cf8f6
hlreev <hannah.leah@email.com>
2026-07-25 18:52:54 -0500

    hello world is working!

---
+++ modules/main.py
@@ -0,0 +1,16 @@
+'''
+main.py: Core of the game.
+'''
+
+def hello_world():
+    """
+    Initializes the main menu for Console Quest RPG.
+    
+    Parameters:
+        None.
+    
+    Returns:
+        None.
+    """
+
+    print("Hello world!")
---
+++ run.py
@@ -0,0 +1,11 @@
+'''
+File: run.py
+Description: Initializes the game in the console/terminal.
+Author: Hannah Reeves
+Date: 2026-07-18
+'''
+
+from modules.main import hello_world
+
+if __name__ == "__main__":
+    hello_world()
